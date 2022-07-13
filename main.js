b94bbb9cabbce8cb80c62eef2710db84e7a1a6ba
AleOrtiz <aleortizjusto3@gmail.com>
2022-07-12 22:15:27 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,59 @@
+
+
+
+// const edadUsuariosRegistrados = [19, 15, 32, 58, 12, 17]
+
+
+
+// let sumador = 0;
+// for (let posicion = 0; posicion <6; posicion++){
+//     sumador = sumador + edadUsuariosRegistrados [posicion]
+
+// }
+// const promedio = sumador/6;
+
+
+// if (promedio >=18)
+//     alert('La mayoria de los usuarios registrados son mayores de edad, la edad promedio es de ' + promedio)
+
+//     else alert('La mayoria de los usuarios registrados son mayores de edad, la edad promedio es de ' + promedio)
+
+
+
+
+const edadUsuariosRegistrados = [19, 15, 32, 58, 12, 17]
+
+
+
+let sumador = 0;
+for (let posicion = 0; posicion < 6; posicion++) {
+    sumador = sumador + edadUsuariosRegistrados[posicion]
+
+}
+const promedio = sumador / 6
+
+
+if (promedio >= 18)
+    alert('La mayoria de los usuarios registrados son mayores de edad, la edad promedio es de ' + promedio)
+
+else alert('La mayoria de los usuarios registrados son mayores de edad, la edad promedio es de ' + promedio)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
